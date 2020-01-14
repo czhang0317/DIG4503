a52821051136dc1a317e3a0cc7ae0f2021f41959
czhang0317 <33127175+czhang0317@users.noreply.github.com>
2020-01-13 19:13:38 -0500

    Example Week1

---
+++ example/Header.js
@@ -0,0 +1,10 @@
+const Component = require("./Component.js");
+
+class Header extends Component {
+    constructor(){
+        super();
+        this.text = "Oh, hi!";
+    }
+}
+
+module.exports = Header;
